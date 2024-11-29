e5996649bb6feee03bf61ccff0c83a5776022aba
sahanasreenath <85185016+sahanasreenath@users.noreply.github.com>
2024-11-29 17:20:47 -0600

    Create variables.tf

---
+++ postgres-sql-module/variables.tf
@@ -0,0 +1,29 @@
+variable "labels" {
+  description = "Labels to Apply"
+  type        = map(string)
+}
+
+variable "disk_size" {
+  type        = number
+  description = "disk size in GBs"
+  default     = 20
+}
+
+variable "vpc_id" {
+  description = "VPC ID"
+  type        = string
+}
+
+variable "project_name" {
+  description = "Customer Name"
+  type        = string
+}
+
+variable "production" {
+  description = "True if this is a production cluster"
+  type        = bool
+}
+
+variable "address_space_name" {
+  description = "Name of reserved address space used for the PostgreSQL private network service"
+}
