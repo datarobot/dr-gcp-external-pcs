edefbd1b9ae221323ea349856e2eabe4b83b393b
sahanasreenath <85185016+sahanasreenath@users.noreply.github.com>
2024-11-29 23:03:44 -0600

    Update variables.tf

--- redis-cache-module/variables.tf
+++ redis-cache-module/variables.tf
@@ -19,6 +19,17 @@ variable "vpc_id" {
   type        = string
 }
 
+variable "vpc_network_id" {
+  description = "VPC Network ID"
+  type        = string
+}
+
 variable "redis_reserved_ip_range" {
   description = "Reserved address space used for the Redis private network service"
 }
+
+variable "redis_subnet_cidr" {
+  description = "Redis CIDR Range"
+  type        = string
+}
+
