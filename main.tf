f72237eb02cd5e047cf5faeb18ee5e7ef08e4fb0
sahanasreenath <85185016+sahanasreenath@users.noreply.github.com>
2024-11-29 19:57:34 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ module "redis" {
   source                    = "./redis-cache-module"
   project_name              = local.project_name
   vpc_id                    = local.vpc_id
-  redis_ address_space_name = local.redis_address_name
+  redis_address_space_name = local.redis_address_name
   labels                    = local.default_labels
 }
 
